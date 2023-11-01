ce8200aafcfec18dbb4426b8d657b90a9f38fe57
Mustafa <mostafakhaled0787314@gmail.com>
2023-11-01 22:19:26 +0200

    timer

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import styles from "./Timer.module.css";
 
 function Timer() {
-  return <div className={styles.time}>Timer</div>;
+  const [progress, setProgress] = useState(50);
+
+  return (
+    <div className={styles.timer}>
+      <div
+        className={styles["circle-progress"]}
+        style={{
+          background: `conic-gradient(var(--secondary) ${progress}%, transparent ${progress}%)`,
+        }}>
+        <div>some thinsg</div>
+      </div>
+    </div>
+  );
 }
 
 export default Timer;
